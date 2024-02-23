3f34cf101b5130e343f35266600fea81d6f810ac
jamilshafayat <jamilshafayat8@gmail.com>
2024-02-23 14:54:48 +0600

    Subject module deleted

--- src/main/java/io/javabrains/courseapidata/subject/SubjectRepository.java
+++
@@ -1,6 +0,0 @@
-package io.javabrains.courseapidata.subject;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface SubjectRepository extends CrudRepository<Subject, String> {
-}
