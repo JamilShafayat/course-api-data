6e917f7ac230b87a2609e5c86ffde3fdae00d9fa
jamilshafayat <jamilshafayat8@gmail.com>
2024-03-23 01:23:56 +0600

    Module modificccccccation

--- src/main/java/io/javabrains/courseapidata/destination/DestinationController.java
+++ src/main/java/io/javabrains/courseapidata/destination/DestinationController.java
@@ -12,29 +12,29 @@ public class DestinationController {
     @Autowired
     private DestinationService courseService;
 
-    @RequestMapping("/topics/{id}/courses")
+    @RequestMapping("/destinations/{id}/courses")
     public List<Destination> getAllCourses(@PathVariable String id) {
         return courseService.getAllCourses(id);
     }
 
-    @RequestMapping("/topics/{topicId}/courses/{id}")
+    @RequestMapping("/destinations/{topicId}/courses/{id}")
     public Destination getCourse(@PathVariable String id) {
         return courseService.getCourse(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses")
+    @RequestMapping(method = RequestMethod.POST, value = "/destinations/{topicId}/courses")
     public Destination addCourse(@RequestBody Destination course, @PathVariable String topicId) {
         course.setTopic(new Topic(topicId, "", ""));
         return courseService.addCourse(course);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses/{id}")
+    @RequestMapping(method = RequestMethod.PUT, value = "/destinations/{topicId}/courses/{id}")
     public Destination updateCourse(@RequestBody Destination course, @PathVariable String topicId, @PathVariable String id) {
         course.setTopic(new Topic(topicId, "", ""));
         return courseService.updateCourse(course);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{topicId}/courses/{id}")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/destinations/{topicId}/courses/{id}")
     public void deleteCourse(@PathVariable String id) {
         courseService.deleteCourse(id);
     }
