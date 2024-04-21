8dcba30fa80e5a04e2a27446ed353bd86c350a25
JamilShafayat <jamilshafayat8@gmail.com>
2024-04-21 18:57:26 +0600

    Test run in simec desk, delete Destination and Transportation part.

--- src/main/java/io/javabrains/courseapidata/destination/DestinationRepository.java
+++
@@ -1,9 +0,0 @@
-package io.javabrains.courseapidata.destination;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface DestinationRepository extends CrudRepository<Destination, String> {
-    public List<Destination> findByTopicId(String topicId);
-}
--- src/main/java/io/javabrains/courseapidata/transportation/TransportationRepository.java
+++
@@ -1,9 +0,0 @@
-package io.javabrains.courseapidata.transportation;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface TransportationRepository extends CrudRepository<Destination, String> {
-    public List<Destination> findByTopicId(String topicId);
-}
