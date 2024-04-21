5b6010bae29fb588aceab8b693a06e714630f7a9
jamilshafayat <jamilshafayat8@gmail.com>
2024-04-22 01:02:16 +0600

    Transportation part.

---
+++ src/main/java/io/javabrains/courseapidata/transportation/TransportationRepository.java
@@ -0,0 +1,6 @@
+package io.javabrains.courseapidata.transportation;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TransportationRepository extends CrudRepository<Transportation, String> {
+}
